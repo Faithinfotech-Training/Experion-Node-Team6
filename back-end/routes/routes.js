d182c4f638702b672434f597c095bc831773a7d9
Ravikanth <princeravikanth001@gmail.com>
2021-11-26 17:40:24 +0530

    Merge branch 'Development/REST_API' of https://github.com/Faithinfotech-Training/Experion-Node-Team6 into Development/REST_API

--- back-end/routes/routes.js
+++ back-end/routes/routes.js
@@ -3,12 +3,10 @@ const express = require('express');
 
 const router = express.Router();
 
+const courseRoutes=require('./course.route');
 
 
-const resourceEnquiryRoutes = require('./resourceEnquiry.route')
-
-router.use('/resourceEnquiry', resourceEnquiryRoutes);
-
+router.use('/course',courseRoutes);
 const course_enquiryRoutes = require('./course_enquirys.route');
 router.use('/course_enquirys', course_enquiryRoutes);
 module.exports = router;
