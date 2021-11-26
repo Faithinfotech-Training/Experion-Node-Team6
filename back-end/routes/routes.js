be313f8091b59973e65fcae0d4b7e22c873a36c0
Jibymol Joseph <jibymol.joseph@experionglobal.local>
2021-11-26 16:59:31 +0530

    Routes updated new

--- back-end/routes/routes.js
+++ back-end/routes/routes.js
@@ -7,9 +7,9 @@ const router = express.Router();
 
 const resourceEnquiryRoutes = require('./resourceEnquiry.route')
 
-
 router.use('/resourceEnquiry', resourceEnquiryRoutes);
 
 const course_enquiryRoutes = require('./course_enquirys.route');
 router.use('/course_enquirys', course_enquiryRoutes);
 module.exports = router;
+
